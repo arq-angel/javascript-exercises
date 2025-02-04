03f9ecd2f85af718a0ed09f58f82c55ec03784dd
Arjun Thapa <arjunthapa2058@gmail.com>
2025-02-04 18:09:46 +1100

    complete 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(word, count) {
+    if (count < 0) {
+        return "ERROR";
+    }
 
+
+    let result = ""
+
+    for (i=0; i<count; i++){
+        result += word;
+    }
+
+    return result;
 };
 
 // Do not edit below this line

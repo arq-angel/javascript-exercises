42d5cba15f6a0c51a068d83556d0ee5d2a6049ab
Arjun Thapa <arjunthapa2058@gmail.com>
2025-02-04 18:27:41 +1100

    complete 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,20 @@
-const reverseString = function() {
+const reverseString = function(word) {
 
+    // Easier Way
+    // return word.split("").reverse().join("");
+
+    // My way
+    let arr = word.split("");
+    let count = arr.length;
+    let newArr = [];    
+
+    for(i = count-1; i >= 0; i--) {
+        newArr.push(arr[i]);
+    }
+
+    let newWord = newArr.join("");
+
+    return newWord;
 };
 
 // Do not edit below this line

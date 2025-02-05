16815369d9ee33c4a1a76dc1c799554132c68ab7
Arjun Thapa <arjunthapa2058@gmail.com>
2025-02-05 23:21:51 +1100

    complete 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,23 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+
+  let newTemp = (temp - 32) * 5/9;
+
+  if (newTemp === 0) {
+    return 0;
+  }
+
+  return parseFloat(newTemp.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+
+  let newTemp = (temp * 9/5) + 32;
+
+  if (newTemp === 0) {
+    return 0;
+  }
+
+  return parseFloat(newTemp.toFixed(1));
 };
 
 // Do not edit below this line
